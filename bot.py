06dc3ec309206e2c4cbc1796d3d5f97cfae06d82
RacketeerGames <zhy0224@gmail.com>
2023-02-13 20:08:53 -0800

    Added Match Data

--- bot.py
+++ bot.py
@@ -4,6 +4,14 @@ from discord.ext import commands, tasks
 from datetime import datetime, time, timedelta
 import tracker
 
+'''
+img_link = "https://i.etsystatic.com/24589365/r/il/5a7378/3329746240/il_fullxfull.3329746240_dxht.jpg"
+
+                for player in myTeamList:
+                    if name.split('#')[0].lower() == player.name.lower():
+                        img_link = tracker.get_image_link(player.character)'''
+
+
 def run_discord_bot():
     TOKEN = open(".env", "r").read()
     client = commands.Bot(command_prefix='!', intents=discord.Intents.default())
@@ -19,6 +27,9 @@ def run_discord_bot():
             current_users.append(content[0])
             current_discord_ids.append(int(content[1]))
             current_dm_count.append(int(content[2]))
+
+
+
 
     @client.event
     async def on_ready():
@@ -89,6 +100,101 @@ def run_discord_bot():
         msg += "```"
         await interaction.response.send_message(msg)
 
+    @client.tree.command(name = "get_match_history")
+    @app_commands.describe(name = "Prints match history of a certain user", mode = "What mode? (competitive, unrated, deathmatch, spikerush, swiftplay, escalation, replication)")
+    async def get_match_history(interaction: discord.Interaction, name: str, mode: str='competitive'):
+        await interaction.response.defer()
+        resp = tracker.get_match_history(name, mode)
+        if resp == "broke":
+            await interaction.response.send_message("Fuck you your inputs are wrong")
+        else:
+            embed = discord.Embed(
+                colour=discord.Colour.dark_purple(),
+                description=f"{ mode.capitalize() } match history (5 games)",
+                title=f"{ name }",
+            )
+
+            embed.set_footer(text="DM Teddygat0r#8612 for suggestions to bot")
+            embed.set_author(name="Teddygat0r", url="https://github.com/Teddygat0r/pst-tracker-bot")
+            embed.set_thumbnail(url="https://www.valorantpcdownload.com/wp-content/uploads/2021/08/TX_CompetitiveTier_Large_11.png")
+
+            for index, game in zip(range(len(resp)), resp):
+                if name.split('#')[0].lower() in [player.name.lower() for player in game.players.red]:
+                    myteam = game.teams.red
+                    myTeamList = game.players.red
+                    theirTeamList = game.players.blue
+                else: 
+                    myteam = game.teams.blue
+                    myTeamList = game.players.blue
+                    theirTeamList = game.players.red
+
+                teamliststr = "```"
+                for p1, p2 in zip(myTeamList, theirTeamList):
+                    teamliststr += f"{ p1.name.ljust(16) } |  { p2.name.ljust(16) }\n"
+                teamliststr += "```"
+
+                embed.add_field(name=f"{ game.metadata.map }   |   { myteam.rounds_won } : { myteam.rounds_lost }", value=teamliststr, inline=False)
+
+            await interaction.followup.send(embed=embed)
+
+    @client.tree.command(name = "detail_match")
+    @app_commands.describe(name = "Select User", mode = "What mode? (competitive, unrated, deathmatch, spikerush, swiftplay, escalation, replication)", index="Index of game (1-10)")
+    async def detail_match(interaction: discord.Interaction, name: str, mode: str='competitive', index: int=1):
+        await interaction.response.defer()
+        if index > 10 or index < 1:
+            await interaction.response.send_message("Fuck you index 1-10 only")
+        resp = tracker.get_match_history(name, mode, index)
+        if resp == "broke":
+            await interaction.response.send_message("Fuck you your inputs are wrong")
+        else:
+            game = resp[index - 1]
+            me = game.players.all_players[0]
+
+            if name.split('#')[0].lower() in [player.name.lower() for player in game.players.red]:
+                myteam = game.teams.red
+                myTeamList = game.players.red
+                theirTeamList = game.players.blue
+            else: 
+                myteam = game.teams.blue
+                myTeamList = game.players.blue
+                theirTeamList = game.players.red
+            for player in myTeamList:
+                if name.split('#')[0].lower() == player.name.lower():
+                    me = player
+                    img_link = tracker.get_image_link(player.character)
+            
+            headshots = round(me.stats.headshots / (me.stats.bodyshots + me.stats.headshots + me.stats.legshots) * 100, 2 )
+            kast = 0
+
+            for rounds in game.rounds:
+                kast += 1 if tracker.getKAST(me.puuid, rounds) else 0
+            
+            kast = round(kast / len(game.rounds) * 100, 2)
+
+            embed = discord.Embed(
+                colour=discord.Colour.dark_purple(),
+                description=f"{ me.stats.kills } / { me.stats.deaths } / { me.stats.assists } / HS: { headshots }% / KAST: { kast }%",
+                title=f"{ mode.capitalize() }  |  { game.metadata.map }",
+            )
+
+            for count, rounds in enumerate(game.rounds):
+                for pl in rounds.player_stats:
+                    if pl.player_puuid == me.puuid:
+                        myRoundStats = pl
+                    
+                assists, traded, survived = tracker.statKast(me.puuid, rounds)
+
+                val = f"```{ myRoundStats.kills } / { assists } / Survived: { survived } / Traded: { traded }\n"
+                val += f"{ myRoundStats.economy.weapon.name } + { myRoundStats.economy.armor.name } | ${ myRoundStats.economy.spent + myRoundStats.economy.remaining } - ${ myRoundStats.economy.spent } = ${ myRoundStats.economy.remaining }"
+                val += "```"
+
+                embed.add_field(name=f"Round { count }", value=val, inline=False)
+
+            embed.set_footer(text="DM Teddygat0r#8612 for suggestions to bot")
+            embed.set_author(name="Teddygat0r", url="https://github.com/Teddygat0r/pst-tracker-bot")
+            embed.set_thumbnail(url=img_link)
+
+            await interaction.followup.send(embed=embed)
 
     @tasks.loop(minutes=1)
     async def once_a_day():
--- tracker.py
+++ tracker.py
@@ -1,6 +1,8 @@
 import requests
 import urllib.parse
 from bs4 import BeautifulSoup as bs4
+import json
+import valo_api
 
 def get_match_count(user: str, mode="competitive", allSeason=True):
     try:
@@ -16,4 +18,63 @@ def get_match_count(user: str, mode="competitive", allSeason=True):
     except Exception as e:
         print(e)
         return "broke"
+
+def get_match_history(user: str, mode="competitive", allSeason=True, region="na", count=5):
+    try:
+        return valo_api.endpoints.get_match_history_by_name_v3(region, user.split('#')[0], user.split('#')[1], game_mode=mode, size=count)
+        
+    except Exception as e:
+        print(e)
+        return "broke"
+    
+def get_image_link(agent: str):
+    operators = json.load(open('operator_data.json'))['data']
+    for operator in operators:
+        if agent == operator['displayName']:
+            return operator['displayIcon']
+
+def getKAST(puuid, round):
+    pdeathTime = -1
+    murderer = None
+    for player in round.player_stats:
+        for kill in player.kill_events:
+            if kill.killer_puuid == puuid:
+                return True
+            if kill.victim_puuid == murderer and kill.kill_time_in_round - pdeathTime < 3000:
+                return True
+            if puuid in [ast.assistant_puuid for ast in kill.assistants]:
+                return True
+            if kill.victim_puuid == puuid:
+                pdeathTime = kill.kill_time_in_round
+                murderer = kill.killer_puuid
+    
+    if pdeathTime == -1:
+        return True
+
+    return False
+
+def statKast(puuid, round):
+    pdeathTime = -1
+    murderer = None
+    traded = False
+    survived = True
+    assists = 0
+    for player in round.player_stats:
+        for kill in player.kill_events:
+            if kill.victim_puuid == murderer and kill.kill_time_in_round - pdeathTime < 3000:
+                traded = True
+            if puuid in [ast.assistant_puuid for ast in kill.assistants]:
+                assists += 1
+            if kill.victim_puuid == puuid:
+                pdeathTime = kill.kill_time_in_round
+                murderer = kill.killer_puuid
+                survived = False
+    
+    return assists, traded, survived
+
+    return False
+
+def refresh_operator_data():
+    with open('operator_data.json', 'w') as myf:
+        myf.write(requests.get('https://valorant-api.com/v1/agents/?isPlayableCharacter=true'))
 
